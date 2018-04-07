a21ad11b7a5c5a0fc0b94f81973874684a6a0ad0
Dmitriy Ryajov <dryajov@gmail.com>
2018-04-06 18:45:33 -0600

    feat: use pull-mplex (renamed from pull-plex)

--- src/muxer.js
+++ src/muxer.js
@@ -3,6 +3,10 @@
 const EventEmitter = require('events').EventEmitter
 const Connection = require('interface-connection').Connection
 const setImmediate = require('async/setImmediate')
+const debug = require('debug')
+
+const log = debug('libp2p-mplex:muxer')
+log.err = debug('libp2p-mplex:muxer:error')
 
 const MULTIPLEX_CODEC = require('./codec')
 
@@ -19,7 +23,10 @@ class MultiplexMuxer extends EventEmitter {
     multiplex.on('error', (err) => this.emit('error', err))
 
     multiplex.on('stream', (stream) => {
-      stream.once('error', (() => {}))
+      // install default error handler so that it doesn't throw
+      stream.on('error', (err) => {
+        log.err('receiver stream errored', err)
+      })
       this.emit('stream', new Connection(stream, this.conn))
     })
   }
@@ -28,9 +35,11 @@ class MultiplexMuxer extends EventEmitter {
   newStream (callback) {
     callback = callback || noop
     let stream = this.multiplex.createStream()
-
+    // install default error handler so that it doesn't throw
+    stream.on('error', (err) => {
+      log.err('initiator stream errored', err)
+    })
     const conn = new Connection(stream, this.conn)
-    stream.openChan()
     setImmediate(() => callback(null, conn))
     return conn
   }
